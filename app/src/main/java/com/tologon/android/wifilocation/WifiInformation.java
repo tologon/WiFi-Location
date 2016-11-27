9d996a58c7a47a7f1d4d2f24fd0b4de62f911dfe
Tologon Eshimkanov <teshimkanov@gmail.com>
2016-11-27 18:09:05 -0500

    Add ability to update RSSI value every X number of milliseconds.

--- app/src/main/java/com/tologon/android/wifilocation/WifiInformation.java
+++ app/src/main/java/com/tologon/android/wifilocation/WifiInformation.java
@@ -3,24 +3,57 @@ package com.tologon.android.wifilocation;
 import android.content.Context;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 public class WifiInformation extends AppCompatActivity {
+    private Handler mHandler;
+    TextView WiFiView;
+    Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wifi_information);
-        TextView RSSIView = (TextView) findViewById(R.id.RSSIView);
-        int RSSI = getSignalStrength(this);
-        RSSIView.setText(String.valueOf(RSSI));
+        WiFiView = (TextView) findViewById(R.id.WiFiView);
+
+        mHandler = new Handler();
+        startRepeatingTask();
     }
 
-    public int getSignalStrength(Context context) {
+    public String getSignalStrength() {
         WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
         WifiInfo wifiInfo = wifiManager.getConnectionInfo();
-        return wifiInfo.getRssi();
+        return String.valueOf(wifiInfo.getRssi());
+    }
+
+    @Override
+    public  void onDestroy() {
+        super.onDestroy();
+        stopRepeatingTask();
+    }
+
+    Runnable mStatusChecker = new Runnable() {
+        int mInterval = 10; // 1 millisecond = 1,000 value
+
+        @Override
+        public void run() {
+            try {
+                String RSSI = getSignalStrength();
+                WiFiView.setText(RSSI);
+            } finally {
+                mHandler.postDelayed(mStatusChecker, mInterval);
+            }
+        }
+    };
+
+    void startRepeatingTask() {
+        mStatusChecker.run();
+    }
+
+    void stopRepeatingTask() {
+        mHandler.removeCallbacks(mStatusChecker);
     }
 }
