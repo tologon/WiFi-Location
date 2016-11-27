5a35eaa0a9740855329c004777bebe481b97cfb4
Tologon Eshimkanov <teshimkanov@gmail.com>
2016-11-27 14:06:33 -0500

    Initial commit for WiFi Location (Android app) project.

---
+++ app/src/main/java/com/tologon/android/wifilocation/WifiInformation.java
@@ -0,0 +1,13 @@
+package com.tologon.android.wifilocation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WifiInformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wifi_information);
+    }
+}
