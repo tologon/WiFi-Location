21e0406abbec4285538b5a288cae8d4bc8852f8c
Tologon Eshimkanov <teshimkanov@gmail.com>
2016-11-27 15:38:47 -0500

    Add basic display of WiFi signal strength (RSSI value).

--- app/src/main/java/com/tologon/android/wifilocation/WifiInformation.java
+++ app/src/main/java/com/tologon/android/wifilocation/WifiInformation.java
@@ -1,7 +1,11 @@
 package com.tologon.android.wifilocation;
 
+import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class WifiInformation extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class WifiInformation extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wifi_information);
+        TextView RSSIView = (TextView) findViewById(R.id.RSSIView);
+        int RSSI = getSignalStrength(this);
+        RSSIView.setText(String.valueOf(RSSI));
+    }
+
+    public int getSignalStrength(Context context) {
+        WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+        return wifiInfo.getRssi();
     }
 }
